2d381a8fb0438a40e21b12f501a0b5c894c30a4d
Denilson Salas G <denilsonsalguz@gmail.com>
2023-03-24 08:22:15 -0500

    add firebase config

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+//importamos firebase
+import './services/firebase.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />,
+)
---
+++ src/services/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7bXkSqoK5ADbbsLW-K33b-ekOLSpowJM",
+  authDomain: "crud-fb-app.firebaseapp.com",
+  projectId: "crud-fb-app",
+  storageBucket: "crud-fb-app.appspot.com",
+  messagingSenderId: "674508994274",
+  appId: "1:674508994274:web:aae1ab137b9fa5755f50bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = app.firestore();
